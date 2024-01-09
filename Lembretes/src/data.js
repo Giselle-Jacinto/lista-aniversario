1c39493cb17dd8494b6f32e8803977d72b11a3b8
Giselle jacinto <gisellejacinto1219@gmail.com>
2024-01-08 21:07:48 -0300

    Projeto - UX/UI
    1° Atividade de Tutoria Ligada ao Projeto - UX/UI

---
+++ Lembretes/src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Giselle Jacinto',
+    age: 40,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+  },
+  {
+    id: 2,
+    name: 'Luanna Jacinto',
+    age: 13,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+  },
+  {
+    id: 3,
+    name: 'Jonatas',
+    age: 28,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+  },
+  {
+    id: 4,
+    name: 'Carlinhos',
+    age: 42,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+  },
+  {
+    id: 5,
+    name: 'Catrina the Cat',
+    age: 31,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+  },
+]
